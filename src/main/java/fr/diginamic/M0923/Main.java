16adb0753401c6a5f256ec5865ad9c46181d2be1
rijarandria <rijandrisolo@gmail.com>
2023-11-23 16:29:55 +0100

    Rectif affichage

--- src/main/java/fr/diginamic/M0923/Main.java
+++ src/main/java/fr/diginamic/M0923/Main.java
@@ -15,7 +15,9 @@ public class Main {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("demo-jpa");
         EntityManager em = emf.createEntityManager();
         em.getTransaction().begin();
+        System.out.println("La connexion");
         System.out.println(emf);
+        System.out.println("-------------------------------------------------------------");
 /*//Create new livre
         Livre livreToCreate = new Livre("Recette", "Francis Navarre");
         em.persist(livreToCreate);*/
@@ -23,22 +25,28 @@ public class Main {
         //Modifcation d'un livre
         Livre livreAModifier = em.find(Livre.class, 5);
         if (null != livreAModifier) {
-            livreAModifier.setTitre("Du plaisir dans la cuisine");
+            livreAModifier.setTitre("Que Du plaisir dans la cuisine");
             Livre livreToFind = em.find(Livre.class,5);
+            System.out.println("Modifcation d'un livre");
             System.out.println("Changement :"+livreToFind);
+            System.out.println("-------------------------------------------------------------");
         }
       //Récupération d'un livre par titre
         TypedQuery<Livre> livreFindTitre = (TypedQuery<Livre>) em.createQuery("SELECT h FROM Livre h WHERE h.titre='Germinal'", Livre.class);
         if(null!=livreFindTitre) {
             Livre resultTitre = livreFindTitre.getSingleResult();
+            System.out.println("Récupération d'un livre par titre");
             System.out.println(resultTitre);
+            System.out.println("-------------------------------------------------------------");
         }
 
   //Récupération d'un livre par Auteur
         TypedQuery<Livre> livreFindAuthor =  em.createQuery("SELECT h FROM Livre h WHERE h.auteur='Francis Navarre'", Livre.class);
         if(null!=livreFindAuthor ) {
             Livre resultAthor = livreFindAuthor.getSingleResult();
+            System.out.println("Récupération d'un livre par Auteur");
             System.out.println(resultAthor);
+            System.out.println("-------------------------------------------------------------");
         }
 
 
@@ -47,9 +55,11 @@ public class Main {
         if(null!=livreFind ) {
 
            List<Livre> resultLivre = livreFind.getResultList();
-
+            System.out.println("Affichage des livres avec titre et auteur");
+            System.out.println("-------------------------------------------------------------");
             for(int i = 0;i<resultLivre.size();i++){
                 System.out.println(resultLivre.get(i));
+                System.out.println("-------------------------------------------------------------");
             }
         }
         //Suppression
